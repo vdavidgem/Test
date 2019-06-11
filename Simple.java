91d99c3f64cf8a3be08341b72fc3a89d39f244aa
vdavidgem <47139426+vdavidgem@users.noreply.github.com>
2019-06-11 11:51:38 +0530

    Add files via upload

---
+++ Simple.java
@@ -0,0 +1,9 @@
+public class Simple{
+public static void main(String args[]){
+System.out.println("Hello Java");
+System.out.println("hello Jenkins");
+System.out.println("Hello world");
+System.out.println("HDFS");
+System.out.println("HI@@XXXXXXX");
+}
+}
